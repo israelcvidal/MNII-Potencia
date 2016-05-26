8431cf6d534f46a4dd10a9d167ee44c6195e6210
israelcvidal <israelcvidal@gmail.com>
2016-05-25 23:05:37 -0300

    uma hora da certo

--- src/PotenciaComDeslocamento.java
+++ src/PotenciaComDeslocamento.java
@@ -67,7 +67,6 @@ public class PotenciaComDeslocamento {
 			
 			//Construindo AModificada
 			AModificada = MatrixOperations.subMatrizEscalar(A, mi);
-		//teste
 		}
 		
 	
--- src/PotenciaInversa.java
+++ src/PotenciaInversa.java
@@ -2,38 +2,20 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class PotenciaInversa {
-	public static void main(String[] args) {
-		ArrayList< ArrayList<Double> > A = new ArrayList<ArrayList<Double>>();
-		ArrayList<Double> vk, vk1, phik; 
-		double lambidak, lambidak1;
-		Retorno arquivo = null;
-		Retorno ret;
-				
-		try {
-			arquivo = ManipuladorArquivo.leitor("/Users/israelcvidal/Documents/workspace/MNII-potencia/matriz.txt");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	
-		//Criando e inicializando a matriz A
-		A = arquivo.getMatriz();
-		
+	public static Retorno resolver(ArrayList<ArrayList<Double>> A, ArrayList<Double> vk1, double tolerancia) {
+		double lambidak, lambidak1;
+		ArrayList<Double> vk, phik; 
+		Retorno ret;
+
+
 		//Criando os vetores
 		vk = new ArrayList<Double>();
 		phik = new ArrayList<Double>();
-		
-		// criando e inicializando autovetor inicial
-		vk1 = arquivo.getVetor();
-		
+				
 		//autovalor inicial
 		lambidak1 = 0;
-		
-		System.out.println("MN II - Autovalores e Autovetores \nIsrael de Castro Vidal - 370019");
-		System.out.println("Metodo da Potencia Regular");
-		System.out.println("A Matriz utilizada foi:" + A);
-		System.out.println("O vetor inicial foi: " + arquivo.getVetor());
-		
+
 		do{
 			phik.removeAll(phik);
 			lambidak = lambidak1;
@@ -51,13 +33,48 @@ public class PotenciaInversa {
 			//lambidak1
 			lambidak1 = MatrixOperations.prodescalar(phik, vk1);
 			
-		}while(Math.abs( (lambidak1-lambidak)/lambidak1) > arquivo.getTolerancia());
-		
-		System.out.println("lambidak = " + 1/lambidak1);
-		System.out.println("phik = " + phik);
-		
+		}while(Math.abs( (lambidak1-lambidak)/lambidak1) > tolerancia);
 		
 		ret = new Retorno(1/lambidak1, phik);
+
+		return ret;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		ArrayList< ArrayList<Double> > A = new ArrayList<ArrayList<Double>>();
+		Retorno arquivo = null;
+		ArrayList<Double> vk1;
+		Retorno ret;
+				
+		try {
+			arquivo = ManipuladorArquivo.leitor("/Users/israelcvidal/Documents/workspace/MNII-potencia/matriz.txt");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		//Criando e inicializando a matriz A
+		A = arquivo.getMatriz();
+		
+		// criando e inicializando autovetor inicial
+		vk1 = arquivo.getVetor();
+		
+	
+		System.out.println("MN II - Autovalores e Autovetores \nIsrael de Castro Vidal - 370019");
+		System.out.println("Metodo da Potencia Regular");
+		System.out.println("A Matriz utilizada foi:" + A);
+		System.out.println("O vetor inicial foi: " + arquivo.getVetor());
+		
+		
+		ret = PotenciaInversa.resolver(A, vk1, arquivo.getTolerancia());
+		
+		System.out.println("autovalor = " + ret.getAutovalor());
+		System.out.println("autoveto = " + ret.getAutovetor());
+		
+		
+		
 		
 		try {
 			ManipuladorArquivo.escritor("/Users/israelcvidal/Documents/workspace/MNII-potencia/resultado.txt", ret);
